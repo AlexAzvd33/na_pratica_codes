852f5ebcd1a490b15d8b970d470035556bc89725
Alexandre Azevedo <78055526+AlexAzvd33@users.noreply.github.com>
2022-06-15 21:28:20 -0300

    Add files via upload

---
+++ autogui_arquivos.py
@@ -0,0 +1,35 @@
+import pyautogui
+import  time
+
+
+pyautogui.alert("O código vai começar. Não use nada do seu computador")
+pyautogui.PAUSE = 0.5
+# abrir o Google Drive no meu computador
+pyautogui.press('winleft')
+pyautogui.write('chrome')
+pyautogui.press('enter')
+time.sleep(1)
+pyautogui.write('drive.google.com/drive/u/0/my-drive')
+pyautogui.press('enter')
+
+# entrar na minha area de trabalho
+pyautogui.hotkey('winleft', 'd')
+
+# cliquei no arquivo que eu quero fazr backup e arrastei ele
+pyautogui.moveTo(1306, 35)
+pyautogui.mouseDown()
+pyautogui.moveTo(843, 531)
+
+# enquanto estou arrastando, eu vu mudar para o  Google Drive
+pyautogui.hotkey('alt', 'tab')
+time.sleep(3)
+
+# larguei o arquivo no Google Drive
+pyautogui.mouseUp()
+
+# esperar 5 segundos
+time.sleep(5)
+
+
+pyautogui.alert("O código terminou. Pode voltar a usar seu computador")
+
